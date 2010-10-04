f98d75e5586c6e769bd5c6e53f181fb475401c62
Alex Heaton <Alex@Heaton.me>
2010-10-04 17:15:51 +0100

    update exchange rates at a later date

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,6 +1,22 @@
 class Application < Merb::Controller
-
+  # Flag used for daily ExchangeRate updates
+  
   # Assume alternative layout for ajax or full page requests:
   before Proc.new{ self.class.layout( request.ajax? ? :ajax : :application) }
+
+  before :run_updates
+  
+  # Run exchange rate updates
+  def run_updates
+    @@updates_run ||= Date.today - 1
+    if @@updates_run < Date.today
+      rates = ExchangeRate.all(:new_rate_on_date.lte => Date.today)
+      rates.each do |rate|
+        rate.rate = rate.new_rate
+        rate.save
+      end
+      @@updates_run = Date.today
+    end
+  end
 
 end
