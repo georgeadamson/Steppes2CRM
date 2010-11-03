68eb6020ca21e77a9de0b9600782dc13049654f0
georgeadamson <george.adamson@softwareunity.com>
2010-11-03 15:21:19 +0000

    Better handling of dialog after saving trip elem grid

--- app/controllers/trips.rb
+++ app/controllers/trips.rb
@@ -114,7 +114,7 @@ class Trips < Application
     tour    = Tour.get( params[:tour_id] )
     client  = Client.get( params[:client_id] ) || session.user.most_recent_client
     @client_or_tour = tour || client
-puts @trip.inspect
+
     original_version  = Trip.get( params[:version_of_trip_id] )
     is_new_version    = !original_version.nil?
     
