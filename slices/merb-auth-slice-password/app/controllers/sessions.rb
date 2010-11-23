8e3e5905ed596fb0d60136e7124bd3436d2e537d
georgeadamson <george.adamson@softwareunity.com>
2010-11-23 12:32:51 +0000

    added comment

--- slices/merb-auth-slice-password/app/controllers/sessions.rb
+++ slices/merb-auth-slice-password/app/controllers/sessions.rb
@@ -4,6 +4,7 @@ class MerbAuthSlicePassword::Sessions < MerbAuthSlicePassword::Application
   after :redirect_after_logout, :only => :destroy
 
   # Nov 2010: Removed the message after login to tidy up the ugly url that it caused.
+  # More info: http://groups.google.com/group/merb/browse_thread/thread/bf3d66ba709ce7e3/e0cc38c3105cddfa?lnk=gst&q=merb-auth+url#e0cc38c3105cddfa
 
   private   
   # @overwritable
