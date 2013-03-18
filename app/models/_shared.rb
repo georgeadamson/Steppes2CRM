3d1f70d0997d2ccf00eda377899fab8b0687eb80
George Adamson <george.adamson@softwareunity.com>
2013-03-18 14:06:45 +0000

    Fix: Boolean comparison for Datamapper sort etc

--- app/models/_shared.rb
+++ app/models/_shared.rb
@@ -697,7 +697,15 @@ class Hash
 
 end
 
-
+# Fix <=> comparison of True/False values: (Fixes Datamapper sort on Boolean columns)
+module TrueFalseComparison
+  def <=>(other)
+    raise ArgumentError unless [TrueClass, FalseClass].include?(other.class)
+    other ? (self ? 0 : -1) : (self ? 1 : 0)
+  end
+end
+TrueClass.send(:include, TrueFalseComparison)
+FalseClass.send(:include, TrueFalseComparison)
 
 
 
