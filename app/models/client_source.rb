9ba70b8cc7f18109863fd969ce8f037a62db8bae
George Adamson <george.adamson@softwareunity.com>
2013-06-27 12:55:18 +0100

    Group Client Source lists by splitting them at hyphen and rendering <optgroup>

--- app/models/client_source.rb
+++ app/models/client_source.rb
@@ -10,4 +10,18 @@ class ClientSource
   # Set the default sort order:
   default_scope(:default).update(:order => [:name])
 
+  def major
+    return self.name.split(' - ').first
+  end
+  
+  def minor
+    return self.name.split(' - ').last
+  end
+  
+  # Helper to return a grouped collection, ready for use in a <select> list with <optgroup> tags:
+  # Apparently the (&:major) syntax is equivalent to {|i| i.major}
+  def self.all_grouped
+    return ClientSource.all( :order=>[:name] ).group_by( &:major )
+  end
+  
 end
